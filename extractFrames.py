b70bc526bbb88f0f01a459b0fe3251b486984a70
yavidor <yonatan@avidor.org>
2026-01-09 20:13:26 +0200

    Added thumbnail, audio to video

--- extractFrames.py
+++ extractFrames.py
@@ -4,11 +4,11 @@ vidcap = cv2.VideoCapture(argv[1])
 output_dir = argv[2]
 print(output_dir)
 success, image = vidcap.read()
-count = 0
+count = 1
 success = True
 while success:
-    cv2.imwrite(f"{output_dir}/frames/frame%d.jpg" %
-                count, image)     # save frame as JPEG file
+    cv2.imwrite(f"{output_dir}/frames/{count:06d}.jpg",
+                image)     # save frame as JPEG file
     success, image = vidcap.read()
     if cv2.waitKey(10) == 27:                     # exit if Escape is hit
         break
